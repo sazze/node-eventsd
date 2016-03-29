a3dc837885ed58b621f933d76a0428c9f418d93f
Craig Thayer <cthayer@sazze.com>
2016-03-29 13:40:59 -0700

    add routing key manipulation functions to the protocol library and add and fix tests

--- test/sz-eventsd_test.js
+++ test/sz-eventsd_test.js
@@ -1,4 +1,4 @@
-process.env.SZ_APP_NAME = 'sz-eventsd.test';
+process.env.SZ_APP_NAME = 'eventsd.test';
 process.env.SZ_ENV = 'test';
 
 var chai = require('chai');
@@ -12,7 +12,12 @@ chai.config.includeStack = true;
 
 var EventsD = require('../');
 
-describe('sz-eventsd', function () {
+describe('eventsd', function () {
+  beforeEach(function (done) {
+    timekeeper.freeze(freezed_time);
+    done();
+  });
+
   function createTestServer(port, onMessage) {
     var server = dgram.createSocket('udp4');
 
@@ -50,7 +55,7 @@ describe('sz-eventsd', function () {
         "msg": {
           "test": "hello world"
         },
-        "routingKey": "event.test.env.test.application.sz-eventsdTest"
+        "routingKey": "event.test.env.test.application.eventsdTest"
       };
 
       test_server = createTestServer(port, function (data) {
@@ -68,7 +73,7 @@ describe('sz-eventsd', function () {
       var events = new EventsD();
 
       events.send('test', {test: 'hello world'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
     });
 
@@ -80,7 +85,7 @@ describe('sz-eventsd', function () {
         "msg": {
           "test": "hello world"
         },
-        "routingKey": "event.test.env.test.application.sz-eventsdTest.another.keyValue"
+        "routingKey": "event.test.env.test.application.eventsdTest.another.keyValue"
       };
 
       test_server = createTestServer(port, function (data) {
@@ -98,7 +103,7 @@ describe('sz-eventsd', function () {
       var events = new EventsD();
 
       events.send('test', {test: 'hello world'}, ['another', '{!@#$% key.value ^&*()}'], function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
     });
 
@@ -112,7 +117,7 @@ describe('sz-eventsd', function () {
     //    "msg": {
     //      "test": "hello world"
     //    },
-    //    "routingKey": "event.test.env.test.application.sz-eventsdTest"
+    //    "routingKey": "event.test.env.test.application.eventsdTest"
     //  };
     //
     //  test_server = createTestServer(port, function (data) {
@@ -141,7 +146,7 @@ describe('sz-eventsd', function () {
         "msg": {
           "test": "hello world"
         },
-        "routingKey": "event.test.env.test.application.sz-eventsdTest"
+        "routingKey": "event.test.env.test.application.eventsdTest"
       };
 
       test_server = createTestServer(port, function (data) {
@@ -152,10 +157,10 @@ describe('sz-eventsd', function () {
       var events = new EventsD();
 
       events.send('test', {test: 'hello world'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
 
         events.send('test', {test: 'hello world 2'}, function (err) {
-          expect(err).to.equal(null);
+          expect(err).to.not.be.ok;
           done();
         });
       });
@@ -169,7 +174,7 @@ describe('sz-eventsd', function () {
         "msg": {
           "test": "hello world"
         },
-        "routingKey": "event.test.env.test.application.sz-eventsdTest"
+        "routingKey": "event.test.env.test.application.eventsdTest"
       };
 
       var numSent = 0;
@@ -187,27 +192,27 @@ describe('sz-eventsd', function () {
       var events = new EventsD();
 
       events.send('test', {test: 'hello world'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
 
       events.send('test', {test: 'hello world 2'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
 
       events.send('test', {test: 'hello world 3'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
 
       events.send('test', {test: 'hello world 4'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
 
       events.send('test', {test: 'hello world 5'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
 
       events.send('test', {test: 'hello world 6'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
       });
     });
 
@@ -219,7 +224,7 @@ describe('sz-eventsd', function () {
         "msg": {
           "test": "hello world"
         },
-        "routingKey": "event.test.env.test.application.sz-eventsdTest"
+        "routingKey": "event.test.env.test.application.eventsdTest"
       };
 
       test_server = createTestServer(port, function (data) {
@@ -230,12 +235,12 @@ describe('sz-eventsd', function () {
       var events = new EventsD({autoClose: false});
 
       events.send('test', {test: 'hello world'}, function (err) {
-        expect(err).to.equal(null);
+        expect(err).to.not.be.ok;
 
         events.close();
 
         events.send('test', {test: 'hello world 2'}, function (err) {
-          expect(err).to.equal(null);
+          expect(err).to.not.be.ok;
           events.close();
           done();
         });
@@ -243,13 +248,85 @@ describe('sz-eventsd', function () {
     });
 
     // Teardown
-    afterEach(function () {
+    afterEach(function (done) {
       if (test_server) {
         test_server.close(function () {
+          timekeeper.reset();
+          test_server = null;
+          done();
         });
+      } else {
+        timekeeper.reset();
+        test_server = null;
       }
-      timekeeper.reset();
-      test_server = null;
     });
+  });
+
+  it('escapes routing key parts', function () {
+    var tests = [
+      {test: 'some$^&*(@)Freaky\\Key', expected: 'someFreakyKey'},
+      {test: 'example.com', expected: 'exampleCom'},
+      {test: 'my-key_val1', expected: 'my-key_val1'}
+    ];
+
+    tests.forEach(function (test) {
+      expect(EventsD.escapeRoutingKey(test.test)).to.equal(test.expected);
+    });
+  });
+
+  it('builds routing key', function () {
+    var tests = [
+      {event: 'error', env: 'production', app: 'example', extra: [], expected: 'event.error.env.production.application.example'},
+      {event: 'error', env: 'production', app: 'example', extra: ['errorType', 'warn'], expected: 'event.error.env.production.application.example.errorType.warn'},
+      {event: 'err%or', env: 'prod#uction', app: 'exa!mple', extra: ['error!Type', 'warn!'], expected: 'event.error.env.production.application.example.errorType.warn'},
+      {event: '', env: 'production', app: 'example', extra: [], expected: 'event.unknown.env.production.application.example'},
+      {event: 'error', env: '', app: 'example', extra: [], expected: 'event.error.env.unknown.application.example'},
+      {event: 'error', env: 'production', app: '', extra: [], expected: 'event.error.env.production.application.unknown'},
+      {event: '', env: '', app: '', extra: [], expected: 'event.unknown.env.unknown.application.unknown'},
+      {event: '', env: '', app: '', extra: '', expected: 'event.unknown.env.unknown.application.unknown'}
+    ];
+
+    tests.forEach(function (test) {
+      expect(EventsD.getRoutingKey(test.event, test.env, test.app, test.extra)).to.equal(test.expected);
+    });
+  });
+
+  it('builds eventsd envelope', function () {
+    var tests = [
+      {routingKey: 'event.error.env.production.application.example', msg: {foo: 'bar'}, expected: {time: 'Fri, 02 Mar 2012 11:38:49 GMT', microtime: 0.321, msg: {foo: 'bar'}, routingKey: 'event.error.env.production.application.example'}},
+      {routingKey: 'event.error.env.production.application.example', msg: 'foo/bar', expected: {time: 'Fri, 02 Mar 2012 11:38:49 GMT', microtime: 0.321, msg: 'foo/bar', routingKey: 'event.error.env.production.application.example'}},
+      {routingKey: 'event.error.env.production.application.example', msg: {foo: {bar: 'baz and some #$*@&@^#&!^&*(){} stuff'}}, expected: {time: 'Fri, 02 Mar 2012 11:38:49 GMT', microtime: 0.321, msg: {foo: {bar: 'baz and some #$*@&@^#&!^&*(){} stuff'}}, routingKey: 'event.error.env.production.application.example'}}
+    ];
+
+    tests.forEach(function (test) {
+      var envelope = EventsD.getEnvelope(test.routingKey, test.msg);
+
+      expect(envelope).to.have.property('id');
+      expect(envelope.id).to.be.ok;
+
+      delete envelope.id;
+
+      expect(envelope).to.eql(test.expected);
+    });
+  });
+
+  it('convert routing key to RegExp', function () {
+    var tests = [
+      {key: 'event.error.env.production.application.example', regexp: /^event\.error\.env\.production\.application\.example$/},
+      {key: 'event.error.env.production.application.#', regexp: /^event\.error\.env\.production\.application\..+$/},
+      {key: 'event.error.env.*.application.example', regexp: /^event\.error\.env\.[^\.]+\.application\.example$/},
+      {key: 'event.error.env.*.application.*', regexp: /^event\.error\.env\.[^\.]+\.application\.[^\.]+$/},
+      {key: 'event.error.env.*.application.#', regexp: /^event\.error\.env\.[^\.]+\.application\..+$/},
+      {key: 'event.#.env.*.application.*', regexp: /^event\..+$/}
+    ];
+
+    tests.forEach(function (test) {
+      expect(EventsD.getRoutingKeyRegExp(test.key)).to.eql(test.regexp);
+    });
+  });
+
+  afterEach(function (done) {
+    timekeeper.reset();
+    done();
   });
 });
